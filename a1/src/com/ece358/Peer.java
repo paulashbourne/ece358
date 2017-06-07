81c29c0b8dfbd6607d1c84e8d4f3c8efdcc4a9f1
Marcus Sjolin <marcus.sjolin@live.ca>
2017-06-06 22:48:42 -0700

    Equals and hashcode for peer

--- a1/src/com/ece358/Peer.java
+++ a1/src/com/ece358/Peer.java
@@ -16,4 +16,23 @@ public class Peer {
   public Integer getPort() {
     return port;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Peer peer = (Peer) o;
+
+    if (address != null ? !address.equals(peer.address) : peer.address != null) return false;
+    return port != null ? port.equals(peer.port) : peer.port == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = address != null ? address.hashCode() : 0;
+    result = 31 * result + (port != null ? port.hashCode() : 0);
+    return result;
+  }
 }
